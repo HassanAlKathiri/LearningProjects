0591f44376f632fe3cf44d54ce47ce807a210a4f
HassanAlKathiri <66620484+HassanAlKathiri@users.noreply.github.com>
2020-06-23 07:36:54 +0300

    Add files via upload

---
+++ Robotics/2 DC Motors.ino
@@ -0,0 +1,53 @@
+int enA = 8;
+int in1 = 7;
+int in2 = 6;
+
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+//MOTOR A
+
+digitalWrite(enA,HIGH);
+digitalWrite(in1,LOW);
+digitalWrite(in2,HIGH);
+
+//MOTOR B
+
+
+digitalWrite(enB,HIGH);
+digitalWrite(in3,LOW);
+digitalWrite(in4,HIGH);
+delay(2000);
+
+//MOTOR A
+
+digitalWrite(enA,HIGH);
+digitalWrite(in1,HIGH);
+digitalWrite(in2,LOW);
+
+
+
+//MOTOR B
+digitalWrite(enB,HIGH);
+digitalWrite(in3,HIGH);
+digitalWrite(in4,LOW);
+delay(2000);
+
+
+}
